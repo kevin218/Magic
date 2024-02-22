af862a507f7ef16069f63c9d281a8946c6624928
Kevin Stevenson <kbstevenson@gmail.com>
2024-02-22 10:46:51 -0500

    Update Sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,10 +10,16 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../src/'))
 
+# from meow import __version__
+
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -21,34 +27,48 @@ project = 'MEOW'
 copyright = '2024, Kevin B. Stevenson'
 author = 'Kevin B. Stevenson'
 
+# version = __version__
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_automodapi.automodapi'
-]
-numpydoc_show_class_members = False
+extensions = ['sphinx_rtd_theme', 'sphinx.ext.todo', 'sphinx.ext.viewcode',
+              'sphinx.ext.autodoc', 'numpydoc', 'nbsphinx', 'myst_parser',
+              'sphinx.ext.autosectionlabel', 'sphinx.ext.napoleon']
 
+master_doc = 'index'
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+html_static_path = ["_static"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
-
+pygments_style = 'sphinx'
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+
+import sphinx_rtd_theme
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-# master_doc = 'index'
+html_static_path = []
+
+# Ignoring duplicated section warnings in api file
+suppress_warnings = ['autosectionlabel.*']
+
+# Remove stub file not found warnings
+numpydoc_class_members_toctree = False
