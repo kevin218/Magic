1cd4d4d1994283f0a363345ba3cce10607a4fdc6
Kevin Stevenson <kbstevenson@gmail.com>
2024-02-22 20:30:06 -0500

    new index

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,6 +18,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../../src/'))
 
 # from meow import __version__
+# version = __version__
 
 # -- Project information -----------------------------------------------------
 
@@ -25,17 +26,14 @@ project = 'MEOW'
 copyright = '2024, Kevin B. Stevenson'
 author = 'Kevin B. Stevenson'
 
-# version = __version__
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode',
-              'sphinx.ext.autodoc', 'nbsphinx', 'myst_parser',
+extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx_rtd_theme',
+              'sphinx.ext.autodoc', 'nbsphinx', 'myst_parser', 'numpydoc',
               'sphinx.ext.autosectionlabel', 'sphinx.ext.napoleon']
-# 'sphinx_rtd_theme', 'numpydoc', 
 
 master_doc = 'index'
 # Add any paths that contain templates here, relative to this directory.
@@ -54,12 +52,12 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 
-# import sphinx_rtd_theme
+import sphinx_rtd_theme
 # Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -70,4 +68,4 @@ html_static_path = []
 suppress_warnings = ['autosectionlabel.*']
 
 # Remove stub file not found warnings
-# numpydoc_class_members_toctree = False
+numpydoc_class_members_toctree = False
